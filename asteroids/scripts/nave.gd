ff6190f7f40caed9a2be7c9f690a620a2f750837
Jonatha-Varjao <jonathavarjao@uft.edu.br>
2017-10-06 18:31:41 -0300

    Fisica da movimentação feita
    TODO: impacto e asteroids

--- asteroids/scripts/nave.gd
+++ asteroids/scripts/nave.gd
@@ -1,40 +1,44 @@
 extends Node2D
 
-var vel = 500;
-var pre_tiro = preload("res://scenes/tiro.tscn")
+export var rot_speed = 2.6
+export var thrust = 500
+export var max_vel = 400
+export var friction = 0.65
+
+var screen_size = Vector2()
+var rot = 0
+var pos = Vector2()
+var vel = Vector2()
+var acc = Vector2()
 
 func _ready():
+	screen_size = get_viewport_rect().size
+	pos = screen_size / 2
+	set_pos(pos)
 	set_process(true)
-	pass
-
+	
 func _process(delta):
-	var d = 0
-	var e = 0
-	var c = 0
-	var b = 0
-	
-	if Input.is_action_pressed("baixo"):
-		b = 1
-	if Input.is_action_pressed("cima"):
-		c = -1
-	if Input.is_action_pressed("esquerda"):
-		e = -1
-	if Input.is_action_pressed("direita"):
-		d = 1
-	
-	if Input.is_action_pressed("giro_dir"):
-		rotate(-0.1)
-		#print(get_rot())
 	if Input.is_action_pressed("giro_esq"):
-		rotate(0.1)
-		#print(get_rot())
-	if Input.is_action_pressed("tiro"):
-		var tiro = pre_tiro.instance()
-		tiro.set_global_pos(get_global_pos())
-		tiro.set_dir_x()
-		tiro.set_dir_y()
-		tiro.rotate(get_rot())
-		get_node("../").add_child(tiro)
+		rot += rot_speed * delta	
+	if Input.is_action_pressed("giro_dir"):
+		rot -= rot_speed * delta
+	if Input.is_action_pressed("cima"):
+		acc = Vector2(thrust, 0).rotated(rot)
+	else:
+		acc = Vector2(0, 0)
 		pass
-	set_pos(get_pos() + Vector2(vel*(e+d),vel*(c+b))*delta)	
-	pass
+		
+	acc += vel * -friction
+	vel += acc * delta
+	pos += vel * delta
+	if pos.x > screen_size.width:
+		pos.x = 0
+	if pos.x < 0:
+		pos.x = screen_size.width
+	if pos.y > screen_size.height:
+		pos.y = 0
+	if pos.y < 0:
+		pos.y = screen_size.height
+	set_pos(pos)
+		
+	set_rot(rot - PI/2)
