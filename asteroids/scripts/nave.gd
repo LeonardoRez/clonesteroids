62681f8c10f150e09ea8aa978c46c2268a13aea7
Gamedev <leonardorec1@gmail.com>
2017-10-06 16:51:47 -0300

    correção na movimentação da nave

--- asteroids/scripts/nave.gd
+++ asteroids/scripts/nave.gd
@@ -7,34 +7,43 @@ func _ready():
 	set_process(true)
 	pass
 
+func calc_direcao(centro , rad):
+	var p = [ cos(rad) , sin(rad) ]
+	
+	
+	return
+
 func _process(delta):
 	var d = 0
 	var e = 0
 	var c = 0
 	var b = 0
 	
-	if Input.is_action_pressed("baixo"):
-		b = 1
 	if Input.is_action_pressed("cima"):
-		c = -1
-	if Input.is_action_pressed("esquerda"):
-		e = -1
-	if Input.is_action_pressed("direita"):
-		d = 1
+		c = 1
+
+#	if Input.is_action_pressed("esquerda"):
+#		e = -1
+#	if Input.is_action_pressed("direita"):
+#		d = 1
+#	if Input.is_action_pressed("baixo"):
+#		b = 1
 	
 	if Input.is_action_pressed("giro_dir"):
 		rotate(-0.1)
+		c = 0
 		#print(get_rot())
 	if Input.is_action_pressed("giro_esq"):
 		rotate(0.1)
+		c = 0
 		#print(get_rot())
 	if Input.is_action_pressed("tiro"):
 		var tiro = pre_tiro.instance()
 		tiro.set_global_pos(get_global_pos())
-		tiro.set_dir_x()
-		tiro.set_dir_y()
+		#tiro.set_dir_x()
+		#tiro.set_dir_y()
 		tiro.rotate(get_rot())
 		get_node("../").add_child(tiro)
 		pass
-	set_pos(get_pos() + Vector2(vel*(e+d),vel*(c+b))*delta)	
+	set_pos(get_pos() + Vector2(vel*(cos(get_rot()+PI/2)*c),vel*(sin(get_rot()+PI/2)*-c))*delta)
 	pass
