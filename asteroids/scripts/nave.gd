ad31c48f144defc303e1b3e4572f6583fffef863
Gamedev <leonardorec1@gmail.com>
2017-10-08 16:39:17 -0300

    merge arrumado e intervalo de tiro implementado

--- asteroids/scripts/nave.gd
+++ asteroids/scripts/nave.gd
@@ -1,20 +1,11 @@
 extends Node2D
 
-export var rot_speed = 2.6
-export var thrust = 500
-export var max_vel = 400
-export var friction = 0.65
-
-var screen_size = Vector2()
-var rot = 0
-var pos = Vector2()
-var vel = Vector2()
-var acc = Vector2()
+var vel = 500;
+var pre_tiro = preload("res://scenes/tiro.tscn")
+var ultimo_tiro = 0
+var intervalo_tiro = 0.3
 
 func _ready():
-	screen_size = get_viewport_rect().size
-	pos = screen_size / 2
-	set_pos(pos)
 	set_process(true)
 
 func _process(delta):
@@ -22,16 +13,9 @@ func _process(delta):
 	var e = 0
 	var c = 0
 	var b = 0
-	
+	ultimo_tiro += delta
 	if Input.is_action_pressed("cima"):
 		c = 1
-
-#	if Input.is_action_pressed("esquerda"):
-#		e = -1
-#	if Input.is_action_pressed("direita"):
-#		d = 1
-#	if Input.is_action_pressed("baixo"):
-#		b = 1
 	
 	if Input.is_action_pressed("giro_dir"):
 		rotate(-0.1)
@@ -42,12 +26,14 @@ func _process(delta):
 		c = 0
 		#print(get_rot())
 	if Input.is_action_pressed("tiro"):
-		var tiro = pre_tiro.instance()
-		tiro.set_global_pos(get_global_pos())
-		#tiro.set_dir_x()
-		#tiro.set_dir_y()
-		tiro.rotate(get_rot())
-		get_node("../").add_child(tiro)
+		if ultimo_tiro >= intervalo_tiro:
+			var tiro = pre_tiro.instance()
+			tiro.set_global_pos(get_global_pos())
+			#tiro.set_dir_x()
+			#tiro.set_dir_y()
+			tiro.rotate(get_rot())
+			get_node("../").add_child(tiro)
+			ultimo_tiro = 0
 		pass
 	set_pos(get_pos() + Vector2(vel*(cos(get_rot()+PI/2)*c),vel*(sin(get_rot()+PI/2)*-c))*delta)
 	pass
