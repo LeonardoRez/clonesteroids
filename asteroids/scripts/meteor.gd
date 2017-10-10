c93e7907e291b105324cda1cb7a0a3e5afb632c8
Jonatha-Varjao <jonathavarjao@uft.edu.br>
2017-10-09 21:16:58 -0300

    TODO: implementar o meteoro

---
+++ asteroids/scripts/meteor.gd
@@ -0,0 +1,31 @@
+extends Area2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+var vel   = 0
+var dir_x = 0
+var dir_y = 0
+
+func _ready():
+	vel = rand_range(200,800)
+	set_process(true)
+	
+	pass
+	
+func set_dir_x(x_temp):
+	dir_x = x_temp
+	pass
+func set_dir_y(y_temp):
+	dir_y = y_temp
+	pass
+func _process(delta):
+	set_pos( get_pos() + Vector2( cos(get_rot()+PI/2) * vel , -(sin(get_rot()+PI/2) * vel )  )* delta)
+	if get_pos().x < -30 or get_pos().x > 930:
+		queue_free()
+	if get_pos().y < -30 or get_pos().y > 930:
+		queue_free()
+	
+	pass
+	
